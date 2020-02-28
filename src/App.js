7731b9445a24bddd7d1a60288a4027c238019c9f
Jeremy Gill <jeremygill87@gmail.com>
2020-02-28 14:08:41 -0500

    set up app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import IntroScreen from "./components/IntroScreen";
+import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Testimonials from "./components/Testimonials";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header pageData={pageData}/>
+        <Contact pageData={pageData}/>
+        <IntroScreen pageData={pageData}/>
+        <Footer pageData={pageData}/>
+        <MainPage pageData={pageData}/>
+        <Portfolio pageData={pageData}/>
+        <Resume pageData={pageData}/>
+        <Testimonials pageData={pageData}/>
+      </div>
+    )
+  }
 }
 
 export default App;
