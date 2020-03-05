6dd9cadc2c20ada06d2e832311d2146a731c0dd6
Jeremy Gill <jeremygill87@gmail.com>
2020-03-04 21:01:49 -0500

    images are working

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,12 +9,12 @@ export default class Portfolio extends Component {
           <h1>Check Out Some of My Works.</h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
           {
-            pageData.portfolio && pageData.portfolio.map((item)=>{
+            pageData.portfolio && pageData.portfolio.map((item, i)=>{
               return(
-                <div className="columns portfolio-item">
+                <div className="columns portfolio-item" key={i}>
                   <div className="item-wrap">
                     {/* <a href="#modal-01"> */}
-                    <img src={`${item.imgurl}`} className="item-img" alt="logo"/>
+                    <img src={item.imgurl} className="item-img" alt="logo"/>
                       {/* <div className="overlay">
                         <div className="portfolio-item-meta"> */}
                           <h5>{item.name}</h5>
